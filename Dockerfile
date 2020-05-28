dd0f530d273fe87c4974289bc941f10c8b8c59e7
Haiwei Liu <carllhw@gmail.com>
2020-05-28 15:05:07 +0800

    [IMP] ACTIVITI_VERSION 6.0.0
    Signed-off-by: Haiwei Liu <carllhw@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM tomcat:9-jdk8-adoptopenjdk-openj9
 
-ENV ACTIVITI_VERSION 5.21.0
+ENV ACTIVITI_VERSION 6.0.0
 ENV MYSQL_CONNECTOR_JAVA_VERSION 5.1.49
 
 # Add mirror source
@@ -28,13 +28,13 @@ RUN apt-get update && apt-get install -y \
       rm -rf /var/lib/apt/lists/* && \
     wget -qO /tmp/activiti.zip "https://github.com/Activiti/Activiti/releases/download/activiti-${ACTIVITI_VERSION}/activiti-${ACTIVITI_VERSION}.zip" && \
  	unzip -q /tmp/activiti.zip -d /tmp && \
-	unzip -q "/tmp/activiti-${ACTIVITI_VERSION}/wars/activiti-explorer.war" -d "${CATALINA_HOME}/webapps/activiti-explorer" && \
+	unzip -q "/tmp/activiti-${ACTIVITI_VERSION}/wars/activiti-app.war" -d "${CATALINA_HOME}/webapps/activiti-app" && \
 	unzip -q "/tmp/activiti-${ACTIVITI_VERSION}/wars/activiti-rest.war" -d "${CATALINA_HOME}/webapps/activiti-rest" && \
 	rm -rf /tmp/activiti.zip "/tmp/activiti-${ACTIVITI_VERSION}" && \
     wget -qO /tmp/mysql-connector-java.zip "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}.zip" && \
 	unzip -q /tmp/mysql-connector-java.zip -d /tmp && \
 	cp "/tmp/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}-bin.jar" "${CATALINA_HOME}/webapps/activiti-rest/WEB-INF/lib/" && \
-	cp "/tmp/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}-bin.jar" "${CATALINA_HOME}/webapps/activiti-explorer/WEB-INF/lib/" && \
+	cp "/tmp/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}-bin.jar" "${CATALINA_HOME}/webapps/activiti-app/WEB-INF/lib/" && \
 	rm -rf /tmp/mysql-connector-java.zip "/tmp/mysql-connector-java-${MYSQL_CONNECTOR_JAVA_VERSION}"
 
 ADD assets /assets
--- assets/init.sh
+++ assets/init.sh
@@ -2,33 +2,13 @@
 set -e
 set -x
 
-# mysql db
-DB_TYPE=${DB_TYPE:-}
-DB_HOST=${DB_HOST:-}
-DB_PORT=${DB_PORT:-}
-DB_NAME=${DB_NAME:-}
-DB_USER=${DB_USER:-}
-DB_PASS=${DB_PASS:-}
-
-if [ -z "${DB_HOST}" ]; then
-  echo "WARNING: "
-  echo "  No mysql connection available."
-  echo "  Will work with default H2 in-memory database."
-  unset DB_TYPE
-fi
-
-# use default port number if it is still not set
-case "${DB_TYPE}" in
-  mysql)
-    DB_PORT=${DB_PORT:-3306}
-    sed 's/{{DB_PORT}}/'"${DB_PORT}"'/g' -i /assets/config/db.properties
-    sed 's/{{DB_HOST}}/'"${DB_HOST}"'/g' -i /assets/config/db.properties
-    sed 's/{{DB_NAME}}/'"${DB_NAME}"'/g' -i /assets/config/db.properties
-    sed 's/{{DB_USER}}/'"${DB_USER}"'/g' -i /assets/config/db.properties
-    sed 's/{{DB_PASS}}/'"${DB_PASS}"'/g' -i /assets/config/db.properties
-    cp -f /assets/config/db.properties ${CATALINA_HOME}/webapps/activiti-rest/WEB-INF/classes
-    cp -f /assets/config/db.properties ${CATALINA_HOME}/webapps/activiti-explorer/WEB-INF/classes
-    ;;
-esac
+DB_PORT=${DB_PORT:-3306}
+sed 's/{{DB_PORT}}/'"${DB_PORT}"'/g' -i /assets/config/db.properties /assets/config/activiti-app.properties
+sed 's/{{DB_HOST}}/'"${DB_HOST}"'/g' -i /assets/config/db.properties /assets/config/activiti-app.properties
+sed 's/{{DB_NAME}}/'"${DB_NAME}"'/g' -i /assets/config/db.properties /assets/config/activiti-app.properties
+sed 's/{{DB_USER}}/'"${DB_USER}"'/g' -i /assets/config/db.properties /assets/config/activiti-app.properties
+sed 's/{{DB_PASS}}/'"${DB_PASS}"'/g' -i /assets/config/db.properties /assets/config/activiti-app.properties
+cp -f /assets/config/db.properties ${CATALINA_HOME}/webapps/activiti-rest/WEB-INF/classes
+cp -f /assets/config/activiti-app.properties ${CATALINA_HOME}/webapps/activiti-app/WEB-INF/classes/META-INF/activiti-app
 
 catalina.sh run
